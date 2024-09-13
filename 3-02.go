a7f90219998e6d4ba0fd5e7db044b10ba4351689
SeongukCho <127920849+SeongukCho@users.noreply.github.com>
2024-09-13 18:44:41 +0900

    Add files via upload

---
+++ 3-02.go
@@ -0,0 +1,19 @@
+// 3-02
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+)
+
+func main() {
+	var b []byte
+	var err error
+
+	b, err = ioutil.ReadFile("./hello.txt")
+
+	if err == nil {
+		str1 := string(b[:])
+		fmt.Println(str1)
+	}
+}
