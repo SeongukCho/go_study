ca85b9ec782e1221da4eae9bf94bb5eb84806563
SeongukCho <127920849+SeongukCho@users.noreply.github.com>
2024-10-06 21:42:38 +0900

    Add files via upload

---
+++ 4-05.go
@@ -0,0 +1,14 @@
+// 4-05
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	a := [5]int{32, 29, 78, 16, 81}
+
+	for _, value := range a { //인덱스는 생략, value에 배열 요소 값이 들어감
+		fmt.Println(value)
+	}
+}
