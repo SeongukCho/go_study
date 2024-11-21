388cbc8cf318bdce2d24e5a44329cb14ae890e71
SeongukCho <127920849+SeongukCho@users.noreply.github.com>
2024-11-21 09:10:04 +0900

    Add files via upload

---
+++ 5-04.go
@@ -0,0 +1,15 @@
+// 5-04
+package main
+
+import (
+	"fmt"
+)
+
+func SumAndDiff(a int, b int) (int, int) {
+	return a + b, a - b
+}
+
+func main() {
+	sum, diff := SumAndDiff(6, 2)
+	fmt.Println(sum, diff)
+}
